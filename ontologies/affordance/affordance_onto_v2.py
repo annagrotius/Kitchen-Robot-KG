c43b95c6e5297f981ada103e0db0e6b3b1399a5d
Anna de Groot <annagrotius@gmail.com>
2020-09-13 11:46:32 -0700

    script documented

--- ontologies/affordance/affordance_onto_v2.py
+++ ontologies/affordance/affordance_onto_v2.py
@@ -1,6 +1,6 @@
 """
-This script creates the affordance ontology and saves a closed world and non closed world
-version of the ontology as a owl file format and different graph file formats (.ttl, .nt).
+This script creates the affordance ontology and saves a non closed world
+version of the ontology as an owl file format and different graph file formats (.ttl, .nt).
 """
 
 from owlready2 import *
@@ -41,63 +41,18 @@ def main():
 
         class Action(Thing): pass
 
-        # class Cut(Action): pass
-
-        # class Soft(Quality): pass
-
-        # class SoftAffordance(Affordance): pass
-
     # properties
     with aff:
         class potential_action(ObjectProperty, KitchenEntity>>Action): pass
 
         class quality(ObjectProperty, KitchenEntity>>Quality): pass
 
-        # class implies(ObjectProperty, Quality>>Affordance): pass
-        # problem i have with this is once I create instances of Quality, will all of those be inferred with Affordance. How would
-        # specific qualities be inferred with the respective affordance?
-
         class action_affordance(ObjectProperty, Action>>Affordance): pass
 
-        # class affords(ObjectProperty):
-        #     equivalent_to = PropertyChain([quality, implies, defines])
-
         class affords(KitchenEntity>>Affordance, ObjectProperty):  pass
-            # equivalent_to= [superAction]
-            # equivalent_to= [potential_action, superAction]
-            # equivalent_to = PropertyChain([potential_action, superAction])
-
-    # Affordance.equivalent_to.append(affords.PropertyChain([potential_action, superAction]))
-
-    # apple = KitchenEntity("apple")
-    # cut = Action("cut")
-    # eat = Action("eat")
-    # soft = Quality("soft")
-    # hard = Quality("hard")
-    # eating = Affordance("eating")
-    #
-    # apple.quality = [hard]
-    # apple.potential_action = [eat]
-    # eat.superAction = [eating]
-
-    # print(apple.INDIRECT_affords)
-    # print("work?",apple.affords)
-    #
-    # with aff:
-    #     sync_reasoner(infer_property_values = True)
-    #
-    # print(apple.INDIRECT_affords)
-    # print("work?",apple.affords)
-
 
     # save ontology not closing the world
     onto_to_graph(aff, aff_namespace, 'aff', 'aff_onto_not_closed')
-
-    # # closing world and save this version
-    # classes = [KitchenEntity, Quality, Affordance, Action, quality, color, location, form, implies, affords]
-    # for c in classes:
-    #     close_world(c)
-    # onto_to_graph(aff, aff_namespace, 'aff', 'aff_onto_closed')
 
 
 if __name__ == "__main__":
